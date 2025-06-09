9713eb6daa0d2b409f5ae13a69c4d86276839b6c
numme <nummer14@naver.com>
2025-06-09 13:47:03 +0900

    2nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
-    public static int add(int a, int b) {
-        return a + b;
-    }
-
-    public static int add(int a, int b, int c) {
-        return a + b + c;
-    }
-
     public static void main(String[] args) {
-        System.out.println("a + b = " + add(a:1, int b:2));
-        System.out.println("a + b + c = " + add(a:3, b:4, c:5));
+        String message = "Hello, Java!";
+
+        System.out.println(message.length());
     }
 }
