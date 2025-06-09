0d36ee916ac9bf69311ed0d60a4afa7f28b4577f
numme <nummer14@naver.com>
2025-06-09 15:41:11 +0900

    3nd commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        String message = "Hello, Java!";
+        String greeting = "Hello, Alice";
 
-        System.out.println(message.length());
+        System.out.println(greeting.toLowerCase());
+        System.out.println(greeting.length());
     }
 }
